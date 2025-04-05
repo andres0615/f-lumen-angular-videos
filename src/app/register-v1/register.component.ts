7c53804714b40e21c8649d5cc848c731e3e5a3fc
bafdurango <bafdurango@hotmail.com>
2025-04-04 21:03:17 -0500

    feat: backup del diseño original del componente signup

---
+++ src/app/register-v1/register.component.ts
@@ -0,0 +1,86 @@
+import { Component, OnInit } from '@angular/core';
+import {
+  FormControl,
+  Validators,
+  ReactiveFormsModule,
+  FormBuilder,
+  FormGroup,
+} from '@angular/forms';
+import { matchPassword } from '../confirm-password.directive';
+import { Observable } from 'rxjs';
+import { UserService } from '../user.service';
+import { User } from '../user';
+import { AuthService } from '../auth.service';
+import { Router } from '@angular/router';
+
+@Component({
+  selector: 'app-register',
+  templateUrl: './register.component.html',
+  styleUrls: ['./register.component.css'],
+})
+export class RegisterComponent implements OnInit {
+  //username: string = '';
+  //confirmPassword = {} as FormControl;
+  //password = {} as FormControl;
+  hidePassword: boolean = true;
+  hideConfirmPassword: boolean = true;
+  registerForm = {} as FormGroup;
+
+  constructor(
+    private fb: FormBuilder,
+    public userService: UserService,
+    public authService: AuthService,
+    public router: Router
+  ) {}
+
+  ngOnInit(): void {
+    this.registerForm = this.fb.group(
+      {
+        username: ['', Validators.required],
+        password: ['', Validators.required],
+        confirmPassword: ['', Validators.required],
+      },
+      {
+        validator: matchPassword('password', 'confirmPassword'),
+      }
+    );
+  }
+
+  register(user: User) {
+    if (this.registerForm.valid) {
+      this.userService.storeUser(user).subscribe((user) => {
+        //console.log(user);
+        this.login();
+      });
+    }
+  }
+
+  login(): void {
+    this.authService
+      .login(
+        this.registerForm.controls.username.value,
+        this.registerForm.controls.password.value
+      )
+      .subscribe((res) => {
+        this.authService.setSession(res);
+
+        this.authService.getUser().subscribe((user) => {
+          this.authService.setUser(user);
+
+          this.router.navigate(['/']);
+        });
+
+        // this.router.navigate(['/']);
+      });
+  }
+
+  turnHidePassword(event: Event): void {
+    event.preventDefault();
+    this.hidePassword = !this.hidePassword;
+  }
+
+  turnHideConfirmPassword(event: Event): void {
+    event.preventDefault();
+    this.hideConfirmPassword = !this.hideConfirmPassword;
+  }
+}
